bea28fa60dda832c7699302f84b9d59d1a50e513
Takayuki Kojima <hal4c.lemon@gmail.com>
2019-10-07 17:17:26 +0900

    Add Title n GameMain n Release.

---
+++ Hawk AI/Assets/Source/GeneralObject/GeneralObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralObject : MonoBehaviour
+{
+    void Init()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void Release()
+    {
+
+    }
+
+}
