3812a5ce139f7f286162f687f18fe3a78a08e91b
拓哉 <i.t.picusuke@gmail.com>
2019-10-11 18:58:27 +0900

    Add RespawnPoint cs

---
+++ Hawk AI/Assets/Scenes/intiraymi/RespawnPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnPoint : SingletonMonoBehaviour<RespawnPoint>
+{
+    public void Test()
+    {
+        Debug.Log("singleton");
+    }
+    public void Respawn()
+    {
+
+    }
+}
