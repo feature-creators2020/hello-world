4d5cbd770d99c271edf6996c0dbcd450acf49534
拓哉 <i.t.picusuke@gmail.com>
2019-10-11 12:17:22 +0900

    Add Test_PlayerRespawn cs

---
+++ Hawk AI/Assets/Scenes/intiraymi/Test_PlayerRespawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_PlayerRespawn : GeneralObject
+{
+    public override void GeneralInit()
+    {
+        base.GeneralInit();
+    }
+
+    public override void GeneralUpdate()
+    {
+        base.GeneralUpdate();
+    }
+}
