801fda28fe0ec498cff20d1a7f0056026e3bd1ff
hal-inoue-hiroki <hal.inoue.hiroki@gmail.com>
2019-10-21 18:08:37 +0900

    ネズミの移動処理をステートマシン化

---
+++ Hawk AI/Assets/Source/Player/Mouse/MouseState/SlowDownManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GamepadInput;
+
+
+public class SlowDownManager : CStateBase<MouseStateManager>
+{
+
+    float m_fSlowTime;          // 速度低下状態の効果時間
+
+    public SlowDownManager(MouseStateManager _cOwner) : base(_cOwner) { }
+
+    public override void Enter()
+    {
+        m_fSlowTime = 10f;
+    }
+
+    public override void Execute()
+    {
+        Debug.Log("State:SlowDown");
+
+        var playerNo = m_cOwner.GamePadIndex;
+        var keyState = GamePad.GetState(playerNo, false);
+
+        // ゲームパッドの入力情報取得
+        m_cOwner.inputHorizontal = 0f;
+        m_cOwner.inputVertical = 0f;
+
+        m_cOwner.inputHorizontal = keyState.LeftStickAxis.x;
+        m_cOwner.inputVertical = keyState.LeftStickAxis.y;
+
+        // カメラの方向から、x-z平面の単位ベクトルを取得
+        Vector3 cameraForward = Vector3.Scale(m_cOwner.targetCamera.transform.forward, new Vector3(1, 0, 1)).normalized;
+
+        // 移動量
+        Vector3 moveForward = cameraForward * m_cOwner.inputVertical + m_cOwner.targetCamera.transform.right * m_cOwner.inputHorizontal;
+
+        // 速度低下率で割る
+        moveForward = moveForward / m_cOwner.m_fSlowDownRate;
+
+        m_cOwner.transform.position += moveForward * m_cOwner.m_fmoveSpeed * Time.deltaTime;
+
+        // キャラクターの向きを進行方向に
+        if (moveForward != Vector3.zero)
+        {
+            m_cOwner.transform.rotation = Quaternion.LookRotation(moveForward);
+        }
+
+        // 経過時間処理
+        m_fSlowTime -= Time.deltaTime;
+        if(m_fSlowTime <= 0f)
+        {
+            m_cOwner.ChangeState(0, EMouseState.Normal);
+        }
+
+
+        // Debug:ステート変更
+        if (GamePad.GetButtonDown(GamePad.Button.X, playerNo))
+        {
+            m_cOwner.ChangeState(0, EMouseState.Door);
+        }
+        if (GamePad.GetButtonDown(GamePad.Button.Y, playerNo))
+        {
+            m_cOwner.ChangeState(0, EMouseState.Normal);
+        }
+    }
+
+    public override void Exit()
+    {
+
+    }
+
+}
---
+++ Hawk AI/Assets/Source/Player/Mouse/MouseStateManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GamepadInput;
+
+public enum EMouseState
+{
+    Normal,
+    SlowDown,
+    Door
+}
+
+public class MouseStateManager : CStateObjectBase<MouseStateManager, EMouseState>
+{
+
+    public float inputHorizontal;              // コントローラーLスティック横軸情報
+    public float inputVertical;                // コントローラーLスティック縦軸情報
+    public Camera targetCamera;         // 対象のカメラ
+    public GamePad.Index GamePadIndex;  // 対象のコントローラー
+
+    public float m_fmoveSpeed;           // 固定の速度
+    public float m_fSlowDownRate;        // ネズミこうを踏んだとき
+    public float m_fDoorRate;            // ドアの下を通るとき
+    /*{
+        get { return m_fmoveSpeed; }
+        set { m_fmoveSpeed = value; }
+    }*/
+
+    public float RunRate                // 別状態の速度倍率
+    {
+        get { return RunRate; }
+        set { RunRate = value; }
+    }               
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var StateMachine = new CStateMachine<MouseStateManager>();
+        m_cStateMachineList.Add(StateMachine);
+
+        var Normal = new NormalManager(this);
+        var SlowDown = new SlowDownManager(this);
+        var Door = new DoorManager(this);
+
+        m_cStateList.Add(Normal);
+        m_cStateList.Add(SlowDown);
+        m_cStateList.Add(Door);
+
+        m_cStateMachineList[0].ChangeState(m_cStateList[(int)EMouseState.Normal]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        base.Update();
+    }
+}
