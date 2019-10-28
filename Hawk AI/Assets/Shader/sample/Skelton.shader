cba9b38afdfab37ccb1003e006a080904b6f2d56
Takayuki Kojima <hal4c.lemon@gmail.com>
2019-10-28 23:13:23 +0900

    Add Skelton Shader. n Material Contoroller.

---
+++ Hawk AI/Assets/Shader/sample/Skelton.shader
@@ -0,0 +1,65 @@
+﻿Shader "Unlit/Skelton"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+		AlphaTest Off
+		ZTest Always
+		ZWrite On
+		//Blend SrcAlpha SrcAlpha
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+				float3 normal : TEXCOORD0;
+				float3 eye    : TEXCOORD1;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+				float3 normal : TEXCOORD0;
+				float3 eye    : TEXCOORD1;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+				o.normal = normalize(v.normal);
+				o.eye =  -_WorldSpaceCameraPos;
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+				fixed4 col = fixed4(0.3, 0.3, 1.0, 0.3);
+				float3 N = normalize(i.normal);
+				float3 E = normalize(i.eye);	
+				//ハイライトの計算
+				float3 H = normalize(-ObjSpaceLightDir(i.vertex) + E);
+				float S = pow(max(0.0f, dot(N, H)), 30.0f) * 5.0f;
+				col.rgb = normalize(col.rgb + S);
+				return col;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Hawk AI/Assets/Source/Manager/MaterialController/MaterialController.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public interface ISetMaterial : IEventSystemHandler
+{
+    void SetMaterial(GameObject _Object);
+
+    void RevertMaterial(GameObject _Object);
+}
+
+public class MaterialController : GeneralManager,ISetMaterial
+{
+    Dictionary<GameObject, List<Shader>> m_cDicShaders
+    = new Dictionary<GameObject, List<Shader>>();
+
+    Dictionary<GameObject, List<Color>> m_cDicColors
+    = new Dictionary<GameObject, List<Color>>();
+
+    [SerializeField]
+    Material m_cMaterial;   //セットするシェーダーマテリアル
+
+    public void SetMaterial(GameObject _Object)
+    {
+        List<Shader> bufShaders = new List<Shader>();
+
+        // 子のコンポーネントをすべて取得し、ループで回す
+        var SkinnedMeshRend = _Object.GetComponentsInChildren<SkinnedMeshRenderer>();
+
+        //マテリアルに変更
+        foreach (var child in SkinnedMeshRend)
+        {
+            foreach (var material in child.materials)
+            {
+                Color col = Color.cyan;
+
+                if (material != null)
+                {//オブジェクトにマテリアルがセットされている場合
+
+                    //マテリアルの元々設定されている色を取得
+                    if (material.HasProperty("_Color") == true)
+                    {
+                        col = material.GetColor("_Color");
+                        //シェーダーに渡す変数
+                        material.SetColor("_Albedo", col);
+                    }
+                    else
+                    {//error log
+                     // Debug.LogError("material.HasProperty is Not Material Color !");
+                    }
+                    //マテリアルのシェーダー変更
+                    bufShaders.Add(material.shader);
+                    material.shader = m_cMaterial.shader;
+
+                }
+            }
+        }
+
+        m_cDicShaders[_Object] = bufShaders;
+
+    }
+
+    public void RevertMaterial(GameObject _Object)
+    {
+
+        // 子のコンポーネントをすべて取得し、ループで回す
+        var SkinnedMeshRend = _Object.GetComponentsInChildren<SkinnedMeshRenderer>();
+
+        List<Shader> Shaders = m_cDicShaders[_Object];
+
+        int i = 0;
+        foreach (var child in SkinnedMeshRend)
+        {
+            foreach (var material in child.materials)
+            {
+                Color col = Color.cyan;
+
+                if (material != null)
+                {//オブジェクトにマテリアルがセットされている場合
+
+                    material.shader = Shaders[i];
+                    i++;
+                }
+            }
+        }
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            SetMaterial(this.gameObject);
+            
+            
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            RevertMaterial(this.gameObject);
+        }
+
+    }
+
+    //public override void GeneralInit()
+    //{
+    //    base.GeneralInit();
+    //}
+
+    //// Update is called once per frame
+    //public override void GeneralUpdate()
+    //{
+    //    base.GeneralUpdate();
+    //    DebugUpdate();
+    //}
+
+    //public override void GeneralRelease()
+    //{
+    //    base.GeneralRelease();
+    //}
+
+    //public override void DebugUpdate()
+    //{
+    //    base.DebugUpdate();
+    //}
+
+    //public override GameObject GetGameObject(int _ID)
+    //{
+    //    return base.GetGameObject(_ID);
+    //}
+
+    //public override List<GameObject> GetGameObjectsList()
+    //{
+    //    return base.GetGameObjectsList();
+    //}
+
+}
