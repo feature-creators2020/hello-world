8ea68be0f31b695d456b0dba122e71163ddf0e99
Takayuki Kojima <hal4c.lemon@gmail.com>
2019-10-30 16:57:39 +0900

    Fix GameMain TimeCounting/.

--- Hawk AI/Assets/Source/GameMain/GameState/GameMainManager.cs
+++ Hawk AI/Assets/Source/GameMain/GameState/GameMainManager.cs
@@ -7,30 +7,37 @@ public class GameMainManager : CStateBase<GameManager>
 {
     public GameMainManager(GameManager _cOwner) : base(_cOwner) { }
 
+    private GameObject m_cTimerObject = null;
+    private bool m_bCountFlg = false;
+
     // Start is called before the first frame update
     public override void Enter()
     {
         Debug.Log(" NowState : GameMainManager");
 
-        //var obj = ManagerObjectManager.Instance.GetGameObject("FadeManager");
-
-        //ExecuteEvents.Execute<IFadeInterfase>(
-        //target: obj,
-        //eventData: null,
-        //functor: (recieveTarget, y) => recieveTarget.CallFadeStay());
-
-
-        var obj = ManagerObjectManager.Instance.GetGameObject("TimeManager");
+        m_cTimerObject = ManagerObjectManager.Instance.GetGameObject("TimeManager");
 
         ExecuteEvents.Execute<ITimeManager>(
-        target: obj,
+        target: m_cTimerObject,
         eventData: null,
         functor: (recieveTarget, y) => recieveTarget.TimeStart());
+
+        ExecuteEvents.Execute<ITimeManager>(
+        target: m_cTimerObject,
+        eventData: null,
+        functor: (recieveTarget, y) =>  m_bCountFlg = recieveTarget.IsTimeCounting);
 
     }
     public override void Execute()
     {
-        if (Input.anyKeyDown)
+
+        ExecuteEvents.Execute<ITimeManager>(
+        target: m_cTimerObject,
+        eventData: null,
+        functor: (recieveTarget, y) => m_bCountFlg = recieveTarget.IsTimeCounting);
+
+
+        if (m_bCountFlg == false)
         {
             this.m_cOwner.ChangeState(0, EGameState.End);
         }
