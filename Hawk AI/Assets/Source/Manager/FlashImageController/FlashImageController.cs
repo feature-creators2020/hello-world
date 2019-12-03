95c5e5ff575e74742aef59145c451acf99317707
Takayuki Kojima <hal4c.lemon@gmail.com>
2019-12-03 20:53:55 +0900

    Add Title Screen.

---
+++ Hawk AI/Assets/Source/Manager/FlashImageController/FlashImageController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlashImageController : MonoBehaviour
+{
+    [SerializeField]
+    private float FlashRate = 0f;
+
+    private bool m_bContinueFlashFlg = false;
+    private float m_fCountRate = 0f;
+    private Image m_cImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_cImage = this.gameObject.transform.GetChild(0).GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //m_fCountTime += Time.deltaTime;
+        if (m_bContinueFlashFlg == true)
+        {
+            m_fCountRate += FlashRate;
+            m_cImage.color = new Color(m_cImage.color.r, m_cImage.color.g, m_cImage.color.b, m_fCountRate);
+                
+            if(m_fCountRate >= 1f)
+            m_bContinueFlashFlg = false;
+        }
+        else
+        {
+            m_fCountRate -= FlashRate;
+            m_cImage.color = new Color(m_cImage.color.r, m_cImage.color.g, m_cImage.color.b, m_fCountRate);
+
+            if (m_fCountRate <= 0f)
+                m_bContinueFlashFlg = true;
+        }
+
+    }
+
+
+}
