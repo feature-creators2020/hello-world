1dcab92faa18ec6417b04e9b21ebce60322beb9a
Takayuki Kojima <hal4c.lemon@gmail.com>
2019-12-08 01:05:01 +0900

    Add Picture Story Scene FadeIn Out.

--- Hawk AI/Assets/Source/PictureStoryShow/PictureStoryShowManager.cs
+++ Hawk AI/Assets/Source/PictureStoryShow/PictureStoryShowManager.cs
@@ -11,16 +11,20 @@ public class PictureStoryShowManager : MonoBehaviour
 
     [SerializeField]
     private float ChangeSpriteTime;
-
     private float m_fCountTime = 0f;
 
     private int m_nNowPage = 0;
     private int m_nLastPage = 0;
 
+    private bool m_bStateFlg = false;
+
     private Image m_cImage;
 
     [SerializeField]
     private GameObject m_cSceneObj;
+
+    [SerializeField]
+    private GameObject m_cFadeObj;
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +32,13 @@ public class PictureStoryShowManager : MonoBehaviour
         m_nLastPage = SpriteList.Count;
         m_cImage = this.gameObject.transform.GetChild(0).GetComponent<Image>();
         m_cImage.sprite = SpriteList[m_nNowPage];
+
+        ExecuteEvents.Execute<IFadeInterfase>(
+        target: m_cFadeObj,
+        eventData: null,
+        functor: (recieveTarget, y) => recieveTarget.CallFadeIn());
+
+        
     }
 
     // Update is called once per frame
@@ -45,16 +56,30 @@ public class PictureStoryShowManager : MonoBehaviour
             {
                 m_nNowPage = m_nLastPage -1;
 
-                ExecuteEvents.Execute<ISceneInterfase>(
-                target: m_cSceneObj,
+                m_bStateFlg = true;
+                ExecuteEvents.Execute<IFadeInterfase>(
+                target: m_cFadeObj,
                 eventData: null,
-                functor: (recieveTarget, y) => recieveTarget.ChangeStete(ESceneState.Tutorial));
+                functor: (recieveTarget, y) => recieveTarget.CallFadeOut());
 
             }
 
             m_cImage.sprite = SpriteList[m_nNowPage];
 
             Debug.Log("m_nNowPage : " + m_nNowPage);
+        }
+
+
+        if(m_bStateFlg == true)
+        {
+            float flerp = m_cFadeObj.GetComponent<FadeManager>().m_flerpVal;
+            if (flerp >= 1f)
+            {
+                ExecuteEvents.Execute<ISceneInterfase>(
+                target: m_cSceneObj,
+                eventData: null,
+                functor: (recieveTarget, y) => recieveTarget.ChangeStete(ESceneState.Tutorial));
+            }
         }
     }
 }
