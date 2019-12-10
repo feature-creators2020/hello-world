0c59096a2a6e72eb2b79f389c2b39925fc8b310c
Takayuki Kojima <hal4c.lemon@gmail.com>
2019-12-11 00:50:47 +0900

    test Bulsan Effects.

---
+++ Hawk AI/Assets/Source/Utility/Graphics/Smoke/SmokeEffect.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public interface ISmokeEffect : IEventSystemHandler
+{
+    void CallFadeIn();
+    void CallFadeOut();
+
+}
+
+public class SmokeEffect : PostEffect,ISmokeEffect
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void CallFadeIn()
+    {
+
+    }
+
+    public void CallFadeOut()
+    {
+
+
+    }
+
+    protected override void UpdateMaterial()
+    {
+
+
+    }
+
+}
