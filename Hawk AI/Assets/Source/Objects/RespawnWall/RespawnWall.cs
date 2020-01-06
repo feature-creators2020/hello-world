fa729512a8585d1567b9ee315d0b7b61347adcae
hal-inoue-hiroki <hal.inoue.hiroki@gmail.com>
2020-01-06 20:53:20 +0900

    Add RespawnWall

--- Hawk AI/Assets/Source/Objects/RespawnWall/RespawnWall.cs
+++ Hawk AI/Assets/Source/Objects/RespawnWall/RespawnWall.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class RespawnWall : MonoBehaviour
 {
@@ -14,5 +15,41 @@ public class RespawnWall : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Mouse")
+        {
+            ExecuteEvents.Execute<IMouseInterface>(
+                    target: other.gameObject,
+                    eventData: null,
+                    functor: (recieveTarget, y) => recieveTarget.SetRespawn());
+        }
+        if(other.gameObject.tag == "Human")
+        {
+            ExecuteEvents.Execute<IHumanInterface>(
+                    target: other.gameObject,
+                    eventData: null,
+                    functor: (recieveTarget, y) => recieveTarget.SetRespawn());
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Mouse")
+        {
+            ExecuteEvents.Execute<IMouseInterface>(
+                    target: other.gameObject,
+                    eventData: null,
+                    functor: (recieveTarget, y) => recieveTarget.SetRespawn());
+        }
+        if (other.gameObject.tag == "Human")
+        {
+            ExecuteEvents.Execute<IHumanInterface>(
+                    target: other.gameObject,
+                    eventData: null,
+                    functor: (recieveTarget, y) => recieveTarget.SetRespawn());
+        }
     }
 }
